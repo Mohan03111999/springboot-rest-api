d220f5cfae53078edd83e62d0f943599c83dc81e
Mohan03111999 <mohanrajavelu1234@gmail.com>
2023-02-17 09:05:05 +0530

    Added 2 rest API's in Student controller to use path variable

--- springboot-rest-api/src/main/java/com/example/springbootrestapi/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/example/springbootrestapi/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.springbootrestapi.controller;
 
 import com.example.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -24,5 +25,18 @@ public class StudentController {
         students.add(new Student(4,"Rajini", "Kanth"));
         students.add(new Student(5,"Siva", "karthikeyan"));
         return students;
+    }
+
+    //Path variable in a rest API
+    //{id} URI template variable
+    //http://localhost:8080/student/1
+    @GetMapping("student/{id}")
+    public Student getStudent(@PathVariable int id){
+        return new Student(id,"Mohan", "R");
+    }
+    @GetMapping("student/{id}/{first-name}/{last-name}")
+    public Student getStudentWithMultiplePathVariable(@PathVariable int id, @PathVariable("first-name") String firstName,
+                              @PathVariable("last-name") String lastName){
+        return new Student(id,firstName, lastName);
     }
 }
