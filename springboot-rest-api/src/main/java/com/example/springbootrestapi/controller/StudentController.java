8d456fc75f2ce5fc96e81376b6702f5cd1a51394
Mohan03111999 <mohanrajavelu1234@gmail.com>
2023-02-17 08:48:49 +0530

    added rest api in StudentController that returns list of students as JSON

--- springboot-rest-api/src/main/java/com/example/springbootrestapi/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/example/springbootrestapi/controller/StudentController.java
@@ -4,11 +4,25 @@ import com.example.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("student")
     public Student createStudent(){
         Student student = new Student(1,"Mohan", "R");
         return student;
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(1,"Mohan", "R"));
+        students.add(new Student(2,"Bala", "JI"));
+        students.add(new Student(3,"Raja", "Velu"));
+        students.add(new Student(4,"Rajini", "Kanth"));
+        students.add(new Student(5,"Siva", "karthikeyan"));
+        return students;
     }
 }
